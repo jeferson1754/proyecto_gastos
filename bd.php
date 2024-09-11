df52f0249cb2d4ee8454cec247ca56b6044bd752
Jeferson Vargas <jeferson.vargas@revesol.cl>
2024-09-11 12:09:35 -0300

    Se mejora para la conexion de base de datos

--- bd.php
+++ bd.php
@@ -1,9 +1,66 @@
 <?php
 
-$host = "localhost";
-$user = "root";
+/*
+$usuario  = "epiz_32740026";
+$password = "eJWcVk2au5gqD";
+$servidor = "sql208.epizy.com";
+$basededatos = "epiz_32740026_r_user";
+*/
+$usuario  = "root";
 $password = "";
-$database = "gastos";
+$servidor = "localhost";
+$basededatos = "gastos";
+$conexion = mysqli_connect($servidor, $usuario, $password) or die("No se ha podido conectar al Servidor");
+mysqli_query($conexion, "SET SESSION collation_connection ='utf8_unicode_ci'");
+$db = mysqli_select_db($conexion, $basededatos) or die("Upps! Error en conectar a la Base de Datos");
+
+//Linea para los caracteres �
+
+if (!mysqli_set_charset($conexion, "utf8mb4")) {
+    printf("Error loading character set utf8mb4: %s\n", mysqli_error($conn));
+    exit();
+}
+
+if (mysqli_connect_errno()) {
+    die("No se pudo conectar a la base de datos: " . mysqli_connect_error());
+}
+
+$max_queries_per_hour = 500;
+
+$current_time = date("Y-m-d H:i:s", time());
+
+// Consultamos el número de consultas realizadas en la última hora
+$query = "SELECT COUNT(*) AS num_queries FROM consultas WHERE fecha > DATE_SUB(NOW(), INTERVAL 1 HOUR)";
+$result = mysqli_query($conexion, $query);
+
+// Si la consulta falla, lanzamos un error
+if (!$result) {
+    die("La consulta falló: " . mysqli_error($conexion));
+}
+
+$row = mysqli_fetch_assoc($result);
+$num_queries_last_hour = $row["num_queries"];
+
+// Liberamos el resultado de la consulta
+mysqli_free_result($result);
+
+// Si se han superado las consultas permitidas, lanzamos un error
+if ($num_queries_last_hour >= $max_queries_per_hour) {
+    mysqli_close($conexion); // Cerramos la conexión a la base de datos
+    die("Lo siento, has superado el límite de consultas por hora.");
+}
+
+$query = "INSERT INTO consultas (fecha) VALUES ('$current_time')";
+$result = mysqli_query($conexion, $query);
+
+if (!$result) {
+    die("La consulta falló: " . mysqli_error($conexion));
+}
+
+
+$host = $servidor;
+$user = $usuario;
+$database = $basededatos;
 
 date_default_timezone_set('America/Santiago');
 
@@ -42,6 +99,7 @@ if (!$conexion) {
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$database", $user, $password);
+
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     // Consulta para obtener las categorías
